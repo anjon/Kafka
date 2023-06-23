6f2108604ae75ad4b5d5929c9db2d1dedd3ef6e9
Anjon Sarker <anjon.sarker@gmail.com>
2023-06-23 18:25:10 +0200

    Update Kafka-Connect source code

---
+++ Kafka-Basics/java_code/kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -0,0 +1,50 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        // connect to Localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // connect to Conduktor Playground
+        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"77puSrUqFk6EFMdDItAQVt\" password=\"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2F1dGguY29uZHVrdG9yLmlvIiwic291cmNlQXBwbGljYXRpb24iOiJhZG1pbiIsInVzZXJNYWlsIjpudWxsLCJwYXlsb2FkIjp7InZhbGlkRm9yVXNlcm5hbWUiOiI3N3B1U3JVcUZrNkVGTWRESXRBUVZ0Iiwib3JnYW5pemF0aW9uSWQiOjczMzI4LCJ1c2VySWQiOjg1MjUzLCJmb3JFeHBpcmF0aW9uQ2hlY2siOiI0MTZhZGNhNi1hOGQ0LTQ3ODQtODM5My00NGUyM2Q0NmUyYmMifX0.axOl1ZQqMjgpQueoqBoYQeu4QNXYgjO1UeRb88d0DtI\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a Producer Record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        // send data
+        producer.send(producerRecord);
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
