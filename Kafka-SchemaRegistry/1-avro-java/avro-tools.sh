9efbf7fc13d7e712cc1f221313fcf8cb38012a67
Anjon Sarker <anjon.sarker@gmail.com>
2024-11-03 11:18:55 +0100

    Update avro tools

--- Kafka-SchemaRegistry/1-avro-java/avro-tools.sh
+++ Kafka-SchemaRegistry/1-avro-java/avro-tools.sh
@@ -1,5 +1,5 @@
 # put this in any directory you like
-wget http://central.maven.org/maven2/org/apache/avro/avro-tools/1.8.2/avro-tools-1.8.2.jar
+wget https://repo1.maven.org/maven2/org/apache/avro/avro-tools/1.11.4/avro-tools-1.11.4.jar
 
 # run this from our project folder. Make sure ~/avro-tools-1.8.2.jar is your actual avro tools location
 java -jar ~/avro-tools-1.8.2.jar tojson --pretty customer-generic.avro 
