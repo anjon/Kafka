91950403b9eadcc647e59888764ea45e7ac4a82a
Anjon Sarker <anjon.sarker@gmail.com>
2024-11-11 15:37:29 +0100

    Update some old contents

--- Kafka-Admin-Monitoring/0-basic-admin-tools/admin-setup.sh
+++ Kafka-Admin-Monitoring/0-basic-admin-tools/admin-setup.sh
@@ -2,7 +2,7 @@
 sudo yum install -y docker
 sudo systemctl start docker.service
 sudo usermod -a -G docker ec2-user
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 
